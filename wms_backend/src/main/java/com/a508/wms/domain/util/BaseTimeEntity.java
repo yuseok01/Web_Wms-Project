e4394490e9789819b60241fec1cda68ede02a05f
김유석 <kurladbtjr@naver.com>
2024-07-15 17:38:35 +0900

    refactor: 도메인 및 상수 타입 값 변경, 관계 재설정

--- wms_backend/src/main/java/com/a508/wms/domain/util/BaseTimeEntity.java
+++ wms_backend/src/main/java/com/a508/wms/domain/util/BaseTimeEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public class BaseTimeEntity {
+public class BaseTimeEntity  {
 
     @CreatedDate
     @Column(updatable = false)
