5c05ac802b790a31c624f43a87bf6874aae3164f
rnjsdlf9302 <rnjsdlf9302@gmail.com>
2024-07-16 14:18:11 +0900

    feat: Product 단일 조회

--- wms_backend/src/main/java/com/a508/wms/controller/ProductController.java
+++ wms_backend/src/main/java/com/a508/wms/controller/ProductController.java
@@ -2,11 +2,14 @@ package com.a508.wms.controller;
 
 
 import com.a508.wms.domain.Product;
+import com.a508.wms.dto.ProductInfos;
 import com.a508.wms.dto.ProductResponse;
 import com.a508.wms.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,25 @@ public class ProductController {
         this.productService=productService;
     }
 
+    /**
+     * 서비스내의 모든 상품 데이터를 가져오는 기능
+     * @return
+     * results": [
+     *         {
+     *             "quantity": 0,
+     *             "expirationDate": null,
+     *             "comment": null,
+     *             "productDetail": {
+     *                 "barcode": 111111111111,
+     *                 "name": "상품1",
+     *                 "size": 1,
+     *                 "unit": 12,
+     *                 "originalPrice": 2000,
+     *                 "sellingPrice": 20000
+     *             }
+     *         },...
+     *  ]
+     */
     @GetMapping
     public ResponseEntity<ProductResponse> getProducts() {
         return ResponseEntity.ok(
@@ -26,5 +48,29 @@ public class ProductController {
                 .results(productService.findAll())
                 .build()
         );
+    }
+
+
+    /**
+     * 해당 product의 id에 해당하는
+     * @param id : product의 id
+     * @return
+     * {
+     *     "quantity": 0,
+     *     "expirationDate": null,
+     *     "comment": null,
+     *     "productDetail": {
+     *         "barcode": 111111111111,
+     *         "name": "상품1",
+     *         "size": 1,
+     *         "unit": 12,
+     *         "originalPrice": 2000,
+     *         "sellingPrice": 20000
+     *     }
+     * }
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductInfos> getProduct(@PathVariable Long id){
+        return ResponseEntity.ok(productService.findById(id));
     }
 }
--- wms_backend/src/main/java/com/a508/wms/service/ProductService.java
+++ wms_backend/src/main/java/com/a508/wms/service/ProductService.java
@@ -5,6 +5,7 @@ import com.a508.wms.domain.ProductDetail;
 import com.a508.wms.dto.ProductDetailResponse;
 import com.a508.wms.dto.ProductInfos;
 import com.a508.wms.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,11 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+
+    /**
+     * DB에서 가져온 Product의 데이터를 Stream을 통해 Dto로 전환하여 반환해주는 기능
+     * @return List<ProductInfos> Product의 데이터를 가진 DTO들의 List
+     */
     public List<ProductInfos> findAll(){
         final List<Product> products=productRepository.findAll();
         return products.stream().map(product -> ProductInfos.builder()
@@ -26,6 +32,11 @@ public class ProductService {
             .toList();
     }
 
+    /**
+     *
+     * @param productDetail : ProductDetail의 데이터를 Dto로 변환해주는 기능
+     * @return ProductDetailResponse : ProductDetail의 데이터를 가진 DTO
+     */
     public ProductDetailResponse getProductDetail(ProductDetail productDetail){
         return ProductDetailResponse.builder()
             .barcode(productDetail.getBarcode())
@@ -34,6 +45,22 @@ public class ProductService {
             .unit(productDetail.getUnit())
             .originalPrice(productDetail.getOriginalPrice())
             .sellingPrice(productDetail.getSellingPrice())
+            .build();
+    }
+
+    /**
+     * 인자로 들어온 id에 해당하는 Product를 반환하는 기능
+     * id에 해당 하는 데이터가 없다면 예외 발생.
+     * @param id : Product의 id
+     * @return ProductInfos: Product의 데이터를 가진 DTO
+     */
+    public ProductInfos findById(long id){
+        Product product=productRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+
+        return ProductInfos.builder()
+            .comment(product.getComment())
+            .quantity(product.getProductQuantity())
+            .productDetail(getProductDetail(product.getProductDetail()))
             .build();
     }
 }
