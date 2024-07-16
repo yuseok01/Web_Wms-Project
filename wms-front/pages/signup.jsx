c1e9484394573e5ef26153272fcfdbb7c20694a3
Leehansol <gksthf996@naver.com>
2024-07-16 17:29:57 +0900

    feat: 회원가입 기능 추가

--- wms-front/pages/signup.jsx
+++ wms-front/pages/signup.jsx
@@ -1,76 +1,62 @@
-import React from "react";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
-import Email from "@material-ui/icons/Email";
-import People from "@material-ui/icons/People";
-// core components
-import Header from "../../components/Header/Header.js";
-import HeaderLinks from "../../components/Header/HeaderLinks.js";
-import Footer from "../../components/Footer/Footer.js";
-import GridContainer from "../../components/Grid/GridContainer.js";
-import GridItem from "../../components/Grid/GridItem.js";
-import Button from "../../components/CustomButtons/Button.js";
-import Card from "../../components/Card/Card.js";
-import CardBody from "../../components/Card/CardBody.js";
-import CardHeader from "../../components/Card/CardHeader.js";
-import CardFooter from "../../components/Card/CardFooter.js";
-import CustomInput from "../../components/CustomInput/CustomInput.js";
+import styled from "styled-components";
+import Button from "/components/CustomButtons/Button.js";
+// import MailIcon from "../public/svg/mail.svg";
+// import PersonIcon from "../public/svg/person.svg";
+// import PasswordIcon from "../public/svg/password.svg";
 
-import styles from "../../styles/jss/nextjs-material-kit/pages/loginPage.js";
+const Container = styled.form`
 
-const useStyles = makeStyles(styles);
+    width: 568px;
+    padding: 32px;
+    background-color: white;
 
-export default function Plainpage(props) {
-    
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function () {
-    setCardAnimation("");
-  }, 700);
+    .input-wrapper {
+    position: relative;
+    margin-bottom: 16px;
+    input {
+        position: relative;
+        width: 100%;
+        height: 46px;
+        padding: 0 44px 0 11px;
+        border: 1px solid;
+        border-radius: 4px;
+        font-size: 16px;
+        outline: none;
+        ::placeholder {
+            color: gray;
+        }
+      }
+    svg {
+        position: absolute;
+        right: 11px;
+        top: 16px;
+      }
+    }
+    `;
 
-  const classes = useStyles();
-  const { ...rest } = props;
-
-  return (
-    <div>
-        {/** 헤더 */}
-      <Header
-        absolute
-        color="transparent"
-        brand="React.jsx 이식을 위한 준비"
-        rightLinks={<HeaderLinks />}
-        {...rest}
-      />
-      {/** 아래는 배경 사진 */}
-      <div
-        className={classes.pageHeader}
-        style={{
-          backgroundImage: "url('/img/bg7.jpg')",
-          backgroundSize: "cover",
-          backgroundPosition: "top center"
-        }}
-      >
-        {/** 메인 페이지에 해당하는 부분 */}
-        <div className={classes.container}>
-            <h2>여기에 글자 들어갑니까?</h2>
-            <h3>네</h3>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={6} md={4}>
-              <Card className={classes[cardAnimaton]}>
-                  <CardHeader color="primary" className={classes.cardHeader}>
-                  </CardHeader>
-                  <p className={classes.divider}>Or Be Classical</p>
-                  <CardBody>
-                  </CardBody>
-              </Card>
-            </GridItem>
-          </GridContainer>
-        </div>
-        {/** 아래는 푸터 footer */}
-        <Footer whiteFont />
-      </div>
-    </div>
-  );
+// 회원가입 폼
+const SignUp = () => {
+    return (
+        <Container>
+            <div>
+                <h2>회원가입</h2>
+            </div>
+            <div className="input-wrapper">
+                <input type="email" name="email" placeholder="이메일 주소" />
+                {/* <MailIcon /> */}
+            </div>
+            <div className="input-wrapper">
+                <input placeholder="이름(예:홍길동)" />
+                {/* <PersonIcon /> */}
+            </div>
+            <div className="input-wrapper">
+                <input type="password" placeholder="비밀번호 설정하기" />
+                {/* <PasswordIcon /> */}
+            </div>
+            <Button color="primary">회원가입</Button>
+        </Container>
+    )
 }
+
+export default SignUp;
+
