eef614c74d4b1144ad0a19ad9bd4021f6f3594b1
김유석 <kurladbtjr@naver.com>
2024-07-16 11:16:04 +0900

    feat: swagger 설정 추가

---
+++ wms_backend/src/main/java/com/a508/wms/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.a508.wms.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+            .version("v1.0") //버전
+            .title("WMS API") //이름
+            .description("Wms 스웨거 입니다 "); //설명
+        return new OpenAPI()
+            .info(info);
+    }
+
+}
