7d5bc54c37875c9f311e77fe662f821652864acc
Leehansol <gksthf996@naver.com>
2024-07-17 17:27:13 +0900

    feat: 구독 선택 및 상세 페이지 구현

--- wms-front/pages/subscribe.jsx
+++ wms-front/pages/subscribe.jsx
@@ -1,76 +1,55 @@
-import React from "react";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
-import Email from "@material-ui/icons/Email";
-import People from "@material-ui/icons/People";
-// core components
-import Header from "../../components/Header/Header.js";
-import HeaderLinks from "../../components/Header/HeaderLinks.js";
-import Footer from "../../components/Footer/Footer.js";
-import GridContainer from "../../components/Grid/GridContainer.js";
-import GridItem from "../../components/Grid/GridItem.js";
-import Button from "../../components/CustomButtons/Button.js";
-import Card from "../../components/Card/Card.js";
-import CardBody from "../../components/Card/CardBody.js";
-import CardHeader from "../../components/Card/CardHeader.js";
-import CardFooter from "../../components/Card/CardFooter.js";
-import CustomInput from "../../components/CustomInput/CustomInput.js";
+import React from 'react';
+import Button from "@material-ui/core/Button";
+import GridContainer from "/components/Grid/GridContainer.js";
+import GridItem from "@material-ui/core/Grid"; // 필요할 경우 GridItem 사용
+import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
 
-import styles from "../../styles/jss/nextjs-material-kit/pages/loginPage.js";
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    textAlign: 'center', // 텍스트 중앙 정렬
+  },
+  header: {
+    marginBottom: theme.spacing(2),
+  },
+  subheader: {
+    marginBottom: theme.spacing(2),
+  },
+  divider: {
+    width: '60%',
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(3),
+  },
+  button: {
+    margin: theme.spacing(1),
+    width: '300px',
+    height: '50px',
+    color: '#000',
+    borderColor: '#000',
+    backgroundColor: '#fff',
+  },
+}));
 
-const useStyles = makeStyles(styles);
-
-export default function Plainpage(props) {
-    
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function () {
-    setCardAnimation("");
-  }, 700);
-
+// 멤버십 선택 페이지
+// button 의 파라미터로 멤버십 정보 전달
+const Subscribe = () => {
   const classes = useStyles();
-  const { ...rest } = props;
 
   return (
-    <div>
-        {/** 헤더 */}
-      <Header
-        absolute
-        color="transparent"
-        brand="React.jsx 이식을 위한 준비"
-        rightLinks={<HeaderLinks />}
-        {...rest}
-      />
-      {/** 아래는 배경 사진 */}
-      <div
-        className={classes.pageHeader}
-        style={{
-          backgroundImage: "url('/img/bg7.jpg')",
-          backgroundSize: "cover",
-          backgroundPosition: "top center"
-        }}
-      >
-        {/** 메인 페이지에 해당하는 부분 */}
-        <div className={classes.container}>
-            <h2>여기에 글자 들어갑니까?</h2>
-            <h3>네</h3>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={6} md={4}>
-              <Card className={classes[cardAnimaton]}>
-                  <CardHeader color="primary" className={classes.cardHeader}>
-                  </CardHeader>
-                  <p className={classes.divider}>Or Be Classical</p>
-                  <CardBody>
-                  </CardBody>
-              </Card>
-            </GridItem>
-          </GridContainer>
-        </div>
-        {/** 아래는 푸터 footer */}
-        <Footer whiteFont />
-      </div>
-    </div>
+    <GridContainer className={classes.container}>
+      <h2 className={classes.header}>ADN 멤버십</h2>
+      <h3 className={classes.subheader}>재고관리의 혁신을 경험해보세요.</h3>
+      <Divider className={classes.divider} />
+      <Button href='/detail?plan=basic' style={{ color: "brown" }} variant="outlined" className={classes.button}>Basic</Button>
+      <Button href='/detail?plan=standard' style={{ color: "green" }} variant="outlined" className={classes.button}>Standard</Button>
+      <Button href='/detail?plan=premium' style={{ color: "purple" }} variant="outlined" className={classes.button}>Premium</Button>
+    </GridContainer>
   );
 }
+
+export default Subscribe;
