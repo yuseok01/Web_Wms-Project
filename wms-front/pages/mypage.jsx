a57690caf50cb0170c0e4c3fa45c873f0c31f5b1
Leehansol <gksthf996@naver.com>
2024-07-17 17:32:37 +0900

    feat: 마이페이지 구현

--- wms-front/pages/mypage.jsx
+++ wms-front/pages/mypage.jsx
@@ -1,76 +1,50 @@
-import React from "react";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
-import Email from "@material-ui/icons/Email";
-import People from "@material-ui/icons/People";
-// core components
-import Header from "../../components/Header/Header.js";
-import HeaderLinks from "../../components/Header/HeaderLinks.js";
-import Footer from "../../components/Footer/Footer.js";
-import GridContainer from "../../components/Grid/GridContainer.js";
-import GridItem from "../../components/Grid/GridItem.js";
-import Button from "../../components/CustomButtons/Button.js";
-import Card from "../../components/Card/Card.js";
-import CardBody from "../../components/Card/CardBody.js";
-import CardHeader from "../../components/Card/CardHeader.js";
-import CardFooter from "../../components/Card/CardFooter.js";
-import CustomInput from "../../components/CustomInput/CustomInput.js";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from "next/link"
 
-import styles from "../../styles/jss/nextjs-material-kit/pages/loginPage.js";
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    height: '100vh', // 전체 화면 높이에 따라 설정할 수 있습니다.
+  },
+  leftPanel: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    paddingLeft: theme.spacing(2),
+    flex: '2', // 왼쪽 패널의 비율을 2로 설정합니다.
+    backgroundColor: '#f0f0f0', // 예시로 배경색을 설정합니다.
+  },
+  rightPanel: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: '20px',
+    flex: '8', // 오른쪽 패널의 비율을 8로 설정합니다.
+    backgroundColor: '#ffffff', // 예시로 배경색을 설정합니다.
+    textAlign: 'center'
+  },
+}));
 
-const useStyles = makeStyles(styles);
-
-export default function Plainpage(props) {
-    
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function () {
-    setCardAnimation("");
-  }, 700);
-
+const MyPage = () => {
   const classes = useStyles();
-  const { ...rest } = props;
 
   return (
-    <div>
-        {/** 헤더 */}
-      <Header
-        absolute
-        color="transparent"
-        brand="React.jsx 이식을 위한 준비"
-        rightLinks={<HeaderLinks />}
-        {...rest}
-      />
-      {/** 아래는 배경 사진 */}
-      <div
-        className={classes.pageHeader}
-        style={{
-          backgroundImage: "url('/img/bg7.jpg')",
-          backgroundSize: "cover",
-          backgroundPosition: "top center"
-        }}
-      >
-        {/** 메인 페이지에 해당하는 부분 */}
-        <div className={classes.container}>
-            <h2>여기에 글자 들어갑니까?</h2>
-            <h3>네</h3>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={6} md={4}>
-              <Card className={classes[cardAnimaton]}>
-                  <CardHeader color="primary" className={classes.cardHeader}>
-                  </CardHeader>
-                  <p className={classes.divider}>Or Be Classical</p>
-                  <CardBody>
-                  </CardBody>
-              </Card>
-            </GridItem>
-          </GridContainer>
-        </div>
-        {/** 아래는 푸터 footer */}
-        <Footer whiteFont />
+    <div className={classes.container}>
+      <div className={classes.leftPanel}>
+        {/* 왼쪽 패널의 내용 */}
+        <h2>마이페이지</h2>
+        <Link href="/edit"><p>내 정보 수정</p></Link>
+        <Link href="/license"><p>사업자 등록/수정</p></Link>
+        <Link href="/employee"><p>직원 관리</p></Link>
+      </div>
+      <div className={classes.rightPanel}>
+        {/* 오른쪽 패널의 내용 */}
+        <h2>이소희님, 반갑습니다.</h2>
+        <p>이 곳에 기존 개인 정보의 내용이 들어갑니다.</p>
       </div>
     </div>
   );
 }
+
+export default MyPage;
