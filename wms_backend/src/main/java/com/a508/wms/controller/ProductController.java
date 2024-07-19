12a3fb1b5d3e95db922450653343cee4d0cbf7f7
rnjsdlf9302 <rnjsdlf9302@gmail.com>
2024-07-19 09:44:09 +0900

    refactor: Product code

--- wms_backend/src/main/java/com/a508/wms/controller/ProductController.java
+++ wms_backend/src/main/java/com/a508/wms/controller/ProductController.java
@@ -2,10 +2,11 @@ package com.a508.wms.controller;
 
 
 import com.a508.wms.controller.response.BaseSuccessResponse;
-import com.a508.wms.dto.ProductRequest;
-import com.a508.wms.dto.ProductResponse;
+import com.a508.wms.dto.ProductRequestDto;
+import com.a508.wms.dto.ProductResponseDto;
 import com.a508.wms.service.ProductService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,37 +21,34 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
-    private final ProductService productService;
 
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+    private final ProductService productService;
 
     /**
      * (서비스 전체/사업자 별/창고 별/상품 정보별)로의 상품들을 반환하는 기능
-     * @param businessId 사업자 id
-     * @param warehouseId 창고 id
+     *
+     * @param businessId      사업자 id
+     * @param warehouseId     창고 id
      * @param productDetailId 상품정보 id
      * @return
      */
     @GetMapping
-    public BaseSuccessResponse<List<ProductResponse>> findProducts(
+    public BaseSuccessResponse<List<ProductResponseDto>> getProducts(
         @RequestParam(required = false) Long businessId,
         @RequestParam(required = false) Long warehouseId,
         @RequestParam(required = false) Long productDetailId) {
         if (businessId != null) {
             log.info("findProducts businessId: {}", businessId);
             return new BaseSuccessResponse<>(productService.findByBusinessId(businessId));
-        } else if(warehouseId != null){
+        } else if (warehouseId != null) {
             log.info("findProducts warehouseId: {}", warehouseId);
             return new BaseSuccessResponse<>(productService.findByWarehouseId(warehouseId));
-        }
-        else if(productDetailId != null){
+        } else if (productDetailId != null) {
             log.info("findProducts productDetailId: {}", productDetailId);
             return new BaseSuccessResponse<>(productService.findByProductDetailId(productDetailId));
-        }
-        else {
+        } else {
             log.info("findProducts");
             return new BaseSuccessResponse<>(productService.findAll());
         }
@@ -58,36 +56,41 @@ public class ProductController {
 
     /**
      * 특정 상품을 조회하는 기능
+     *
      * @param id 상품 id
      * @return
      */
     @GetMapping("/{id}")
-    public BaseSuccessResponse<ProductResponse> findProduct(@PathVariable Long id){
+    public BaseSuccessResponse<ProductResponseDto> getProduct(@PathVariable Long id) {
         log.info("find product by id: {}", id);
         return new BaseSuccessResponse<>(productService.findById(id));
     }
 
     /**
      * 상품을 등록하는 기능
-     * @param productRequest: Product 데이터
+     *
+     * @param productRequestDto: Product 데이터
      */
     @PostMapping
-    public BaseSuccessResponse<Void> registProduct(@RequestBody ProductRequest productRequest){
-        log.info("regist product");
-        productService.save(productRequest);
+    public BaseSuccessResponse<Void> createProduct(
+        @RequestBody ProductRequestDto productRequestDto) {
+        log.info("create product by productRequest: {}", productRequestDto);
+        productService.save(productRequestDto);
 
         return new BaseSuccessResponse<>(null);
     }
 
     /**
      * 상품을 수정하는 기능
-     * @param id 상품 id
-     * @param productRequest 수정할 상품 정보
+     *
+     * @param id                상품 id
+     * @param productRequestDto 수정할 상품 정보
      */
     @PutMapping("/{id}")
-    public BaseSuccessResponse<Void> updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest){
-        log.info("update product");
-        productService.update(id,productRequest);
+    public BaseSuccessResponse<Void> updateProduct(@PathVariable Long id,
+        @RequestBody ProductRequestDto productRequestDto) {
+        log.info("update product by id: {}", id);
+        productService.update(id, productRequestDto);
 
         return new BaseSuccessResponse<>(null);
     }
@@ -95,11 +98,12 @@ public class ProductController {
 
     /**
      * 상품을 삭제하는 기능 -> 상품의 상태값을 삭제로 변경
+     *
      * @param id: 상품의 id
      */
     @PatchMapping("/{id}")
-    public BaseSuccessResponse<Void> deleteProduct(@PathVariable Long id){
-        log.info("delete Product");
+    public BaseSuccessResponse<Void> deleteProduct(@PathVariable Long id) {
+        log.info("delete product by id: {}", id);
         productService.delete(id);
 
         return new BaseSuccessResponse<>(null);
--- wms_backend/src/main/java/com/a508/wms/dto/ProductRequestDto.java
+++ wms_backend/src/main/java/com/a508/wms/dto/ProductRequestDto.java
@@ -3,15 +3,18 @@ package com.a508.wms.dto;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @Builder
-public class ProductRequest {
+@ToString
+public class ProductRequestDto {
+
     private Long productDetailId;
 
     // 미입력시 -1로 설정(수정시에 필요함)
     @Builder.Default
-    private int productQuantity=-1;
+    private int productQuantity = -1;
     private LocalDateTime expirationDate;
     private String comment;
 }
