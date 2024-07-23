63adfa6164cc03a10f7c3d8727f5244dbcb5bebf
Leehansol <gksthf996@naver.com>
2024-07-23 15:23:27 +0900

    feat: Header 기능 구현

--- wms-front/pages/components.jsx
+++ wms-front/pages/components.jsx
@@ -6,8 +6,9 @@ import Link from "next/link";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // core components
-import Header from "/components/Header/Header.js";
-import HeaderLinks from "/components/Header/HeaderLinks.js";
+import Header from "/components/Header/HomeHeader.js";
+import HeaderLinks from "/components/Header/HomeHeaderLinks.js";
+import BusinessHeaderLink from "/components/Header/BusinessHeaderLinks.js"
 import Footer from "/components/Footer/Footer.js";
 import GridContainer from "/components/Grid/GridContainer.js";
 import GridItem from "/components/Grid/GridItem.js";
@@ -48,7 +49,7 @@ export default function Components(props) {
     <div>
       <Header
         brand="ADN Project for Inventory Manangement"
-        rightLinks={<HeaderLinks />}
+        rightLinks={<BusinessHeaderLink />}
         fixed
         color="transparent"
         changeColorOnScroll={{
