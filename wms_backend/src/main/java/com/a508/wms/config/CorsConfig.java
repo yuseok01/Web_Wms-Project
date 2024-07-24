ee063bba27c27bda918f925c8b9384b8d332ccc0
dongyul <comeme0101@naver.com>
2024-07-24 14:46:49 +0900

    fix: cors 설정 수정

--- wms_backend/src/main/java/com/a508/wms/config/CorsConfig.java
+++ wms_backend/src/main/java/com/a508/wms/config/CorsConfig.java
@@ -1,24 +1,19 @@
 package com.a508.wms.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("*") // 허용할 출처
-                    .allowedMethods("*")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("https://i11a508.p.ssafy.io",
+                "http://i11a508.p.ssafy.io") // 특정 도메인을 명시
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
