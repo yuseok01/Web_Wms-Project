3dccc8657039bc8dbf80135aea288cbc9d16f873
Leehansol <gksthf996@naver.com>
2024-07-24 15:38:24 +0900

    style: 폰트 적용

--- wms-front/pages/_app.js
+++ wms-front/pages/_app.js
@@ -3,6 +3,7 @@ import App from "next/app";
 import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
 import "/styles/scss/nextjs-material-kit.scss?v=1.2.0";
+import '../styles/globals.css';
 // material-kit을 쓰기 위한 글로벌 css 선언
 
 export default class MyApp extends App {
@@ -31,6 +32,9 @@ export default class MyApp extends App {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <title>ADN project Template finding</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR&family=Nanum+Brush+Script&family=Nanum+Gothic+Coding&display=swap" rel="stylesheet"/>
         </Head>
         <SessionProvider session={pageProps.session}>
           <Component {...pageProps} />
