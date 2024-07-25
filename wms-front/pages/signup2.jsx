bb3d1c4640b94aea3373d4ca91129ff3c175f22f
Leehansol <gksthf996@naver.com>
2024-07-25 12:58:30 +0900

    fix: 사용하지 않는 파일 주석

--- wms-front/pages/signup2.jsx
+++ wms-front/pages/signup2.jsx
@@ -1,76 +1,76 @@
-import React from "react";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
-import Email from "@material-ui/icons/Email";
-import People from "@material-ui/icons/People";
-// core components
-import Header from "../../components/Header/Header.js";
-import HeaderLinks from "../../components/Header/HeaderLinks.js";
-import Footer from "../../components/Footer/Footer.js";
-import GridContainer from "../../components/Grid/GridContainer.js";
-import GridItem from "../../components/Grid/GridItem.js";
-import Button from "../../components/CustomButtons/Button.js";
-import Card from "../../components/Card/Card.js";
-import CardBody from "../../components/Card/CardBody.js";
-import CardHeader from "../../components/Card/CardHeader.js";
-import CardFooter from "../../components/Card/CardFooter.js";
-import CustomInput from "../../components/CustomInput/CustomInput.js";
+// import React from "react";
+// // @material-ui/core components
+// import { makeStyles } from "@material-ui/core/styles";
+// import InputAdornment from "@material-ui/core/InputAdornment";
+// import Icon from "@material-ui/core/Icon";
+// // @material-ui/icons
+// import Email from "@material-ui/icons/Email";
+// import People from "@material-ui/icons/People";
+// // core components
+// import Header from "../../components/Header/Header.js";
+// import HeaderLinks from "../../components/Header/HeaderLinks.js";
+// import Footer from "../../components/Footer/Footer.js";
+// import GridContainer from "../../components/Grid/GridContainer.js";
+// import GridItem from "../../components/Grid/GridItem.js";
+// import Button from "../../components/CustomButtons/Button.js";
+// import Card from "../../components/Card/Card.js";
+// import CardBody from "../../components/Card/CardBody.js";
+// import CardHeader from "../../components/Card/CardHeader.js";
+// import CardFooter from "../../components/Card/CardFooter.js";
+// import CustomInput from "../../components/CustomInput/CustomInput.js";
 
-import styles from "../../styles/jss/nextjs-material-kit/pages/loginPage.js";
+// import styles from "../../styles/jss/nextjs-material-kit/pages/loginPage.js";
 
-const useStyles = makeStyles(styles);
+// const useStyles = makeStyles(styles);
 
-export default function Plainpage(props) {
+// export default function Plainpage(props) {
     
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function () {
-    setCardAnimation("");
-  }, 700);
+//   const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+//   setTimeout(function () {
+//     setCardAnimation("");
+//   }, 700);
 
-  const classes = useStyles();
-  const { ...rest } = props;
+//   const classes = useStyles();
+//   const { ...rest } = props;
 
-  return (
-    <div>
-        {/** 헤더 */}
-      <Header
-        absolute
-        color="transparent"
-        brand="React.jsx 이식을 위한 준비"
-        rightLinks={<HeaderLinks />}
-        {...rest}
-      />
-      {/** 아래는 배경 사진 */}
-      <div
-        className={classes.pageHeader}
-        style={{
-          backgroundImage: "url('/img/bg7.jpg')",
-          backgroundSize: "cover",
-          backgroundPosition: "top center"
-        }}
-      >
-        {/** 메인 페이지에 해당하는 부분 */}
-        <div className={classes.container}>
-            <h2>여기에 글자 들어갑니까?</h2>
-            <h3>네</h3>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={6} md={4}>
-              <Card className={classes[cardAnimaton]}>
-                  <CardHeader color="primary" className={classes.cardHeader}>
-                  </CardHeader>
-                  <p className={classes.divider}>Or Be Classical</p>
-                  <CardBody>
-                  </CardBody>
-              </Card>
-            </GridItem>
-          </GridContainer>
-        </div>
-        {/** 아래는 푸터 footer */}
-        <Footer whiteFont />
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//         {/** 헤더 */}
+//       <Header
+//         absolute
+//         color="transparent"
+//         brand="React.jsx 이식을 위한 준비"
+//         rightLinks={<HeaderLinks />}
+//         {...rest}
+//       />
+//       {/** 아래는 배경 사진 */}
+//       <div
+//         className={classes.pageHeader}
+//         style={{
+//           backgroundImage: "url('/img/bg7.jpg')",
+//           backgroundSize: "cover",
+//           backgroundPosition: "top center"
+//         }}
+//       >
+//         {/** 메인 페이지에 해당하는 부분 */}
+//         <div className={classes.container}>
+//             <h2>여기에 글자 들어갑니까?</h2>
+//             <h3>네</h3>
+//           <GridContainer justify="center">
+//             <GridItem xs={12} sm={6} md={4}>
+//               <Card className={classes[cardAnimaton]}>
+//                   <CardHeader color="primary" className={classes.cardHeader}>
+//                   </CardHeader>
+//                   <p className={classes.divider}>Or Be Classical</p>
+//                   <CardBody>
+//                   </CardBody>
+//               </Card>
+//             </GridItem>
+//           </GridContainer>
+//         </div>
+//         {/** 아래는 푸터 footer */}
+//         <Footer whiteFont />
+//       </div>
+//     </div>
+//   );
+// }
