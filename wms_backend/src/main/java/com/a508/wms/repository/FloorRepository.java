0318f5fc0dce72c054af9237414c8f54a281d285
이수완 <ale783@naver.com>
2024-07-29 09:08:30 +0900

    fix: Merge 실수로 올라온 Back-end 파일 삭제

--- wms_backend/src/main/java/com/a508/wms/repository/FloorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.a508.wms.repository;
-
-import com.a508.wms.domain.Floor;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FloorRepository extends JpaRepository<Floor, Long> {
-    List<Floor> findAllByLocationId(Long locationId);
-}
