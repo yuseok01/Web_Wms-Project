3244aa43ad5b5a79ac19886521ed8af6781ffbe8
dongyul <comeme0101@naver.com>
2024-07-29 14:42:55 +0900

    fix: location조회시 floor조회에 대한 N+1문제 발생 fetch join으로 해결

--- wms_backend/src/main/java/com/a508/wms/location/repository/LocationRepository.java
+++ wms_backend/src/main/java/com/a508/wms/location/repository/LocationRepository.java
@@ -8,7 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
+
+    @Query("SELECT l FROM Location l JOIN FETCH l.floors WHERE l.warehouse.id = :warehouseId")
     List<Location> findLocationsByWarehouseId(Long warehouseId);
+
     @Query("SELECT l from Location l where l.name = :name")
     Location findLocationByName(@Param("name") String name);
 }
