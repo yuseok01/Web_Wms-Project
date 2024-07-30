c1dcc1384c6dd2a9ee7e00c16ea5f5137c318ee0
unknown <itgorae.seok@gmail.com>
2024-07-31 07:02:35 +0900

    feat: login/yuseok

--- wms-front/pages/login/index.jsx
+++ wms-front/pages/login/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 import { makeStyles, Button, TextField, Divider, Typography } from '@material-ui/core';
 import GridContainer from '../../components/Grid/GridContainer';
 import GridItem from '../../components/Grid/GridItem';
 import Card from '../../components/Card/Card';
 import CardHeader from '../../components/Card/CardHeader';
 import CardBody from '../../components/Card/CardBody';
-import { signIn } from 'next-auth/react'; // next-auth의 signIn 함수 가져오기
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -57,7 +57,9 @@ const useStyles = makeStyles((theme) => ({
   snsText: {
     margin: theme.spacing(0, 2),
   },
-  
+  title: {
+    marginBottom: theme.spacing(4),
+  },
 }));
 
 export default function Login() {
@@ -66,18 +68,39 @@ export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogoClick = () => {
-    router.push('/');
-  };
-
   const handleLogin = async (e) => {
     e.preventDefault();
-    // 로그인 처리 로직 추가
-    console.log('로그인 시도:', { email, password });
+    try {
+      const response = await axios.post('http://localhost:8080/api/v1/auth/sign-in', {
+        email,
+        password,
+      });
+
+      if (response.status === 200 && response.data.code === 'SU') {
+        alert('로그인 성공!');
+        router.push('/main');
+      }
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 400 && error.response.data.code === 'VF') {
+          alert('로그인에 실패하였습니다.');
+        } else if (error.response.status === 401 && error.response.data.code === 'SF') {
+          alert('로그인 정보가 맞지 않습니다.');
+        } else if (error.response.status === 500 && error.response.data.code === 'DBE') {
+          alert('서버가 불안정합니다.');
+        }
+      } else {
+        alert('로그인에 실패하였습니다.');
+      }
+      router.push('/login');
+    }
   };
 
   return (
     <GridContainer className={classes.container}>
+      <Typography variant="h2" className={classes.title}>
+        Web 재고 관리 시스템
+      </Typography>
       <GridItem xs={12} sm={6} md={4}>
         <Card>
           <CardHeader>
@@ -85,7 +108,7 @@ export default function Login() {
               src="/img/logo.png"
               alt="Logo"
               className={classes.logo}
-              onClick={handleLogoClick}
+              onClick={() => router.push('/')}
             />
           </CardHeader>
           <CardBody>
@@ -103,12 +126,8 @@ export default function Login() {
               <button className="sns-button" onClick={() => signIn('naver')}>
                 <img src="/img/naver-sign-in.png" alt="Naver Sign In" />
               </button>
-              
             </div>
-            <div>
-            <Divider className={classes.divider} />
-            </div> <br />
-          
+            <Divider className={classes.divider} /><br />
             <form onSubmit={handleLogin}>
               <TextField
                 label="이메일 ID"
@@ -118,6 +137,7 @@ export default function Login() {
                 className={classes.textField}
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                required
               />
               <TextField
                 label="패스워드"
@@ -127,6 +147,7 @@ export default function Login() {
                 className={classes.textField}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                required
               />
               <div>
                 <Button type="submit" variant="contained" color="primary" className={classes.button}>
