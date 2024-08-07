efdc3513c75d5a6848a22ea5d7616aabc7b4c570
rnjsdlf9302 <rnjsdlf9302@gmail.com>
2024-08-07 10:54:01 +0900

    refactor: exception 임시 수정

--- wms_backend/src/main/java/com/a508/wms/product/exception/ProductExceptionHandler.java
+++ wms_backend/src/main/java/com/a508/wms/product/exception/ProductExceptionHandler.java
@@ -1,32 +1,11 @@
 package com.a508.wms.product.exception;
 
-import com.a508.wms.util.BaseExceptionResponse;
-import com.a508.wms.util.constant.ResponseEnum;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ProductExceptionHandler {
-    @ExceptionHandler(ProductInvalidRequestException.class)
-    public BaseExceptionResponse inValidRequestError(ProductInvalidRequestException e){
-        ResponseEnum responseEnum=e.getResponseEnum();
-        String exceptionMessage=e.getExceptionMessage();
-        return new ProductException(responseEnum.isSuccess(),responseEnum.getStatusCode(),responseEnum.getHttpStatus(),responseEnum.getMessage()+exceptionMessage);
-    }
 
-    @ExceptionHandler(ProductInvalidDataException.class)
-    public BaseExceptionResponse inValidDataError(ProductInvalidDataException e){
-        ResponseEnum responseEnum=e.getResponseEnum();
-        String exceptionMessage=e.getExceptionMessage();
-        return new ProductException(responseEnum.isSuccess(),responseEnum.getStatusCode(),responseEnum.getHttpStatus(),responseEnum.getMessage()+exceptionMessage);
-    }
-
-    @ExceptionHandler(ProductExportException.class)
-    public BaseExceptionResponse exportError(ProductExportException e){
-        ResponseEnum responseEnum=e.getResponseEnum();
-        String exceptionMessage=e.getExceptionMessage();
-        return new ProductException(responseEnum.isSuccess(),responseEnum.getStatusCode(),responseEnum.getHttpStatus(),responseEnum.getMessage()+exceptionMessage);
-    }
+   
 }
--- wms_backend/src/main/java/com/a508/wms/warehouse/exception/WarehouseExceptionHandler.java
+++ wms_backend/src/main/java/com/a508/wms/warehouse/exception/WarehouseExceptionHandler.java
@@ -1,33 +1,31 @@
 package com.a508.wms.warehouse.exception;
 
-import com.a508.wms.util.BaseExceptionResponse;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class WarehouseExceptionHandler  {
+public class WarehouseExceptionHandler {
 
-    @ExceptionHandler(WarehouseException.WarehouseNotFoundException.class)
-    public BaseExceptionResponse warehouseNotFoundExceptionHandler(WarehouseException.WarehouseNotFoundException e) {
-        return new BaseExceptionResponse(e.isSuccess(),
-                e.getStatusCode(),
-                e.getHttpStatus(),
-                e.packageName + e.getMessage());
-    }
-
-    @ExceptionHandler(WarehouseException.BusinessNotFoundException.class)
-    public BaseExceptionResponse businessNotFoundExceptionHandler(WarehouseException.BusinessNotFoundException e) {
-        return new BaseExceptionResponse(e.isSuccess(),
-                e.getStatusCode(),
-                e.getHttpStatus(),
-                e.packageName + e.getMessage());
-    }
-
-    @ExceptionHandler(WarehouseException.InvalidInputException.class)
-    public BaseExceptionResponse invalidInputExceptionHandler(WarehouseException.InvalidInputException e) {
-        return new BaseExceptionResponse(e.isSuccess(),
-                e.getStatusCode(),
-                e.getHttpStatus(),
-                e.packageName + e.getMessage());
-    }
+//    @ExceptionHandler(WarehouseException.WarehouseNotFoundException.class)
+//    public BaseExceptionResponse warehouseNotFoundExceptionHandler(WarehouseException.WarehouseNotFoundException e) {
+//        return new BaseExceptionResponse(e.isSuccess(),
+//                e.getStatusCode(),
+//                e.getHttpStatus(),
+//                e.packageName + e.getMessage());
+//    }
+//
+//    @ExceptionHandler(WarehouseException.BusinessNotFoundException.class)
+//    public BaseExceptionResponse businessNotFoundExceptionHandler(WarehouseException.BusinessNotFoundException e) {
+//        return new BaseExceptionResponse(e.isSuccess(),
+//                e.getStatusCode(),
+//                e.getHttpStatus(),
+//                e.packageName + e.getMessage());
+//    }
+//
+//    @ExceptionHandler(WarehouseException.InvalidInputException.class)
+//    public BaseExceptionResponse invalidInputExceptionHandler(WarehouseException.InvalidInputException e) {
+//        return new BaseExceptionResponse(e.isSuccess(),
+//                e.getStatusCode(),
+//                e.getHttpStatus(),
+//                e.packageName + e.getMessage());
+//    }
 }
