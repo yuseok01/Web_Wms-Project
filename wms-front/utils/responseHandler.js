47007786ad28415cd56db66f83c53f4ab0ccf49c
unknown <itgorae.seok@gmail.com>
2024-08-08 02:13:36 +0900

    feat: 로그인 로직수정

--- wms-front/utils/responseHandler.js
+++ wms-front/utils/responseHandler.js
@@ -1,26 +1,31 @@
 export const ResponseCode = {
-  SUCCESS: 'SU',
-  VALIDATION_FAIL: 'VF',
-  DUPLICATE: 'DE',
-  SIGN_IN_FAIL: 'SF',
-  CERTIFICATION_FAIL: 'CF',
-  MAIL_FAIL: 'MF',
-  DATABASE_ERROR: 'DBE',
+  SUCCESS: "SU",
+  VALIDATION_FAIL: "VF",
+  DUPLICATE: "DE",
+  SIGN_IN_FAIL: "SF",
+  CERTIFICATION_FAIL: "CF",
+  MAIL_FAIL: "MF",
+  DATABASE_ERROR: "DBE",
 };
 
+// 응답 코드에 따른 메시지를 정의합니다.
 export const ResponseMessage = {
-  [ResponseCode.SUCCESS]: '성공했습니다.',
-  [ResponseCode.VALIDATION_FAIL]: '유효성 검사 실패.',
-  [ResponseCode.DUPLICATE]: '이메일 중복.',
-  [ResponseCode.SIGN_IN_FAIL]: '로그인 정보 불일치.',
-  [ResponseCode.CERTIFICATION_FAIL]: '인증 실패.',
-  [ResponseCode.MAIL_FAIL]: '메일 전송 실패.',
-  [ResponseCode.DATABASE_ERROR]: '데이터베이스 오류.',
+  [ResponseCode.SUCCESS]: "성공했습니다.",
+  [ResponseCode.VALIDATION_FAIL]: "유효성 검사 실패.",
+  [ResponseCode.DUPLICATE]: "이메일 중복 입니다.",
+  [ResponseCode.SIGN_IN_FAIL]: "로그인 정보 불일치.",
+  [ResponseCode.CERTIFICATION_FAIL]: "인증 실패.",
+  [ResponseCode.MAIL_FAIL]: "메일 전송 실패.",
+  [ResponseCode.DATABASE_ERROR]: "데이터베이스 오류.",
 };
 
+// 응답을 처리하는 함수 정의
 export function handleResponse(response) {
-  const { code } = response.data;
-  const message = ResponseMessage[code] || '알 수 없는 오류가 발생했습니다.';
-  const isSuccess = code === ResponseCode.SUCCESS;
-  return { message, isSuccess };
+  const { code, message } = response.data;
+
+  if (ResponseMessage[code]) {
+    return ResponseMessage[code];
+  } else {
+    return "알 수 없는 오류가 발생했습니다.";
+  }
 }
