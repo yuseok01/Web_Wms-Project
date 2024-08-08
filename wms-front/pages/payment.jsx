5d799dcbe0e71c334026fa3f952116c52d1e607f
Leehansol <gksthf996@naver.com>
2024-08-08 15:25:59 +0900

    feat: 결제 완료 모달, 예외 처리

--- wms-front/pages/payment.jsx
+++ wms-front/pages/payment.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button, IconButton } from '@mui/material';
+import { Button, Dialog, DialogContent, DialogTitle, IconButton } from '@mui/material';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 import { useRouter } from 'next/router';
@@ -80,6 +80,15 @@ const useStyles = makeStyles({
     backgroundColor: '#7d4a1a',
     margin: 0
   },
+  modal: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    fontSize: '15px'
+  }
 });
 
 // Iamport 로드
@@ -100,6 +109,10 @@ export default function Payment() {
   const [quantity, setQuantity] = useState(1);
   const pricePerItem = 10000;
 
+  const [openModal, setOpenModal] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
   const handleIncrease = () => {
     setQuantity(quantity + 1);
   };
@@ -109,6 +122,15 @@ export default function Payment() {
       setQuantity(quantity - 1);
     }
   };
+
+  const handleSuccess = () => {
+    setOpenModal(true);
+  }
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    router.push('/components');
+  }
 
   useEffect(() => {
     const initializeIamport = async () => {
@@ -122,49 +144,66 @@ export default function Payment() {
     };
 
     initializeIamport();
+
+    const storedUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("token");
+    
+    if (storedUser && storedToken) {
+      setUser(JSON.parse(storedUser));
+      setIsLoggedIn(true);
+    }
+
   }, []);
 
   // 결제 요청 전송
   const requestPay = () => {
-    const IMP = window.IMP;
-    IMP.request_pay(
-      {
-        pg: 'html5_inicis',
-        pay_method: 'card',
-        merchant_uid: `mid_${new Date().getTime()}`,
-        name: '창고',
-        amount: quantity * pricePerItem,
-        buyer_email: localStorage.user.email,
-        buyer_name: localStorage.user.name,
-        buyer_tel: '010-1111-1111',
-        buyer_addr: '서울시 강남구 테헤란로',
-        buyer_postcode: '12345'
-      },
-      (rsp) => {
-        if (rsp.success) {
-        axios({
-          url: '{서버에서 결제 정보를 받을 엔드포인트}',
-          method: 'post',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          data: {
-            imp_uid: rsp.imp_uid,
-            merchant_uid: rsp.merchant_uid,
-          },
-        })
-          .then(() => {
+
+    if (!isLoggedIn) {
+      alert('로그인이 필요합니다.');
+      router.push('/login');
+      return;
+    } else {
+      const IMP = window.IMP;
+      IMP.request_pay(
+        {
+          pg: 'html5_inicis',
+          pay_method: 'card',
+          merchant_uid: `mid_${new Date().getTime()}`,
+          name: '창고',
+          amount: 100,
+          buyer_email: user.email,
+          buyer_name: user.name,
+          buyer_tel: '010-1111-1111',
+          buyer_addr: '서울시 강남구 테헤란로',
+          buyer_postcode: '12345'
+        },
+        (rsp) => {
+          if (rsp.success) {
+            handleSuccess();
+            // axios({
+            //   url: '{서버에서 결제 정보를 받을 엔드포인트}',
+            //   method: 'post',
+            //   headers: {
+            //     'Content-Type': 'application/json'
+            //   },
+            //   data: {
+            //     imp_uid: rsp.imp_uid,
+            //     merchant_uid: rsp.merchant_uid,
+            //   },
+            // })
+            // .then((data) => {
+            //   handleSuccess(data);
+            // })
+            // .catch (() => {
+            //   router.push('/404');
+            // });
+          } else {
+            console.log(`${rsp.error_msg}`)
             router.push('/components');
-          })
-          .catch (() => {
-            router.push('/404');
-          });
-      } else {
-        console.log(`${rsp.error_msg}`)
-        router.push('/components');
+        }
       }
-    }
-  );
+    );
+  }
 };
 
   return (
@@ -201,6 +240,20 @@ export default function Payment() {
           구매하기
         </Button>
       </div>
+
+      <Dialog open={openModal} onClose={handleCloseModal}>
+        <DialogContent className={classes.modal}>
+          <DialogTitle style={{ textAlign: 'center', padding: '30px', fontWeight: 'bold' }}>결제가 완료되었습니다.</DialogTitle>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingBottom: '20px' }}>
+            <p className={classes.content}>창고 수량 : {quantity} 개</p>
+            <p className={classes.content}>결제 금액 : {quantity * pricePerItem} 원</p>
+          </div>
+          <p style={{ fontSize: '13px'}}>자세한 내역은 마이페이지 - 구매관리 에서 확인하실 수 있습니다.</p>
+          <Button onClick={handleCloseModal} className={classes.purchaseButton}>
+            확인
+          </Button>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
