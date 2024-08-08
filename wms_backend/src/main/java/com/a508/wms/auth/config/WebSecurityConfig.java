226326f71f2bb067c0d66a9d9a5e85038874cc5b
dongyul <comeme0101@naver.com>
2024-08-08 15:46:35 +0900

    fix kakao 경로

--- wms_backend/src/main/java/com/a508/wms/auth/config/WebSecurityConfig.java
+++ wms_backend/src/main/java/com/a508/wms/auth/config/WebSecurityConfig.java
@@ -62,14 +62,15 @@ public class WebSecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authorizeHttpRequests(request -> request
-                .requestMatchers("/api/v1/auth/**","/" , "/api/oauth2/**").permitAll()
-                .requestMatchers("/api/oauth2/authorization/**").permitAll()
+                .requestMatchers("/api/v1/auth/**","/" , "/oauth2/**").permitAll()
+                .requestMatchers("/api/oauth2/**").permitAll()
+                .requestMatchers("/oauth2/authorization/**").permitAll()
                 .requestMatchers("/api/v1/social/**").authenticated()
                 .anyRequest().permitAll()
             )
             .oauth2Login(oauth2 -> oauth2
                 //리다이렉트 엔드포인트 경로 인가코드로 엑세스 토큰 받기
-//                .redirectionEndpoint(endpoint -> endpoint.baseUri("/oauth2/code/*"))
+                .redirectionEndpoint(endpoint -> endpoint.baseUri("/oauth2/code/*"))
                 //사용자 정보 받아오기
                 .userInfoEndpoint(endPoint -> endPoint.userService(oAuth2UserService))
                 .successHandler(customSuccessHandler)  // 성공 핸들러 설정
