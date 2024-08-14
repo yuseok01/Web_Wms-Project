209e835dde663fc610afdf4db6db8bc5db158095
김유석 <kurladbtjr@naver.com>
2024-08-14 15:03:44 +0900

    create warehouse

--- wms-front/styles/jss/nextjs-material-kit/components/cardWarehouse.js
+++ wms-front/styles/jss/nextjs-material-kit/components/cardWarehouse.js
@@ -1,22 +1,22 @@
 const cardStyle = {
-  
   card: {
     border: "0",
     marginBottom: "30px",
     marginTop: "30px",
-    borderRadius: "6px",
+    borderRadius: "30px",
     color: "rgba(0, 0, 0, 0.87)",
     background: "#fff",
     width: "100%",
     boxShadow:
-      "0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12)",
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", // 기본 그림자 설정
     position: "relative",
     display: "flex",
     flexDirection: "column",
     minWidth: "0",
     wordWrap: "break-word",
     fontSize: ".875rem",
-    transition: "all 300ms linear",
+    transition: "transform 0.3s ease, box-shadow 0.3s ease", // 부드러운 전환 효과
+    transformStyle: "preserve-3d", // 3D 효과를 유지하기 위해 추가
   },
   cardPlain: {
     background: "transparent",
@@ -25,12 +25,17 @@ const cardStyle = {
   cardCarousel: {
     overflow: "hidden",
   },
-  gradientHr: {
-    height: "1px",
-    background: "#bbb", // 기본 배경 색상
-    backgroundImage: "linear-gradient(to right, #eee 0%, #777 50%, #eee 100%)", // 표준 문법
-  }
-  
+  frame: {
+    perspective: "1000px", // 입체감을 위한 원근감 추가
+  },
+  light: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    pointerEvents: "none", // 포인터 이벤트를 무시하도록 설정
+  },
 };
 
 export default cardStyle;
