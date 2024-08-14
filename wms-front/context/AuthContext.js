3ba6537c2e77eafe4c4ee5f79b19ab919e9fb517
김유석 <kurladbtjr@naver.com>
2024-08-14 18:53:05 +0900

    feat: user Flow

--- wms-front/context/AuthContext.js
+++ wms-front/context/AuthContext.js
@@ -1,11 +1,8 @@
-// src/context/AuthContext.js
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
-import React, { createContext, useContext, useState } from 'react';
-
-// AuthContext를 생성합니다.
 const AuthContext = createContext();
 
-// Context를 제공하는 컴포넌트를 생성합니다.
 export const AuthProvider = ({ children }) => {
   const [authState, setAuthState] = useState({
     user: null,
@@ -14,22 +11,47 @@ export const AuthProvider = ({ children }) => {
 
   const login = (userData, token) => {
     setAuthState({ user: userData, token });
-
-    localStorage.setItem('token', token); // 토큰을 로컬 저장소에 저장하여 페이지를 새로고침해도 유지
-    localStorage.setItem('user', JSON.stringify(userData)); // 사용자   보를 로컬 저장소에 저장
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(userData));
   };
 
   const logout = () => {
     setAuthState({ user: null, token: null });
-    localStorage.removeItem('token'); // 로그아웃 시 토큰 삭제
-    localStorage.removeItem('user'); // 로그아웃 시 사용자 정보 삭제
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
   };
-  return (   
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const userData = localStorage.getItem('user');
+
+    if (token && userData) {
+      // 로컬 스토리지에서 가져온 사용자 정보로 로그인 함수 호출
+      login(JSON.parse(userData), token);
+    } else {
+      if (token) {
+        axios.get('/api/user', { headers: { Authorization: `Bearer ${token}` } })
+          .then(response => {
+            const userData = response.data;
+            login(userData, token);
+          })
+          .catch(error => {
+            console.error("Failed to fetch user data", error);
+
+            // 에러가 발생했지만 토큰이 유효하지 않다고 확신할 수 없는 경우, logout을 호출하지 않음
+            if (error.response && error.response.status === 401) {
+              logout();
+            }
+          });
+      }
+    }
+  }, []);
+
+  return (
     <AuthContext.Provider value={{ authState, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
-
 
 export const useAuth = () => useContext(AuthContext);
